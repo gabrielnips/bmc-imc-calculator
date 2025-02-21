3f640ea43afd329f2c2aeae436f8d5ee15edd525
Anderson <hzfivem@gmail.com>
2025-02-21 16:15:12 -0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function BMICalculator() {
   const [height, setHeight] = useState("");
   const [gender, setGender] = useState("");
   const [ageGroup, setAgeGroup] = useState("");
+  const [result, setResult] = useState("");
 
   // Função que calcula o IMC usando a fórmula clássica:
   // IMC = peso (kg) / (altura (m)²)
@@ -20,17 +21,22 @@ export default function BMICalculator() {
     return imc.toFixed(2);
   };
 
+  const resultado = {
+    peso: weight,
+    altura: height,
+    imc: calcularIMC(weight, height),
+    genero: gender,
+    faixaEtaria: ageGroup,
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen flex-col gap-5 ">
       <div className="flex justify-between gap-5">
         <form
           className="bg-primary200 py-10 px-14 rounded-lg flex flex-col items-center"
           onSubmit={(e) => {
             e.preventDefault();
-            const imc = calcularIMC(weight, height);
-            alert(
-              `Peso: ${weight} kg | Altura: ${height} m | IMC: ${imc} | Gênero: ${gender} | Faixa Etária: ${ageGroup}`
-            );
+            setResult(JSON.stringify(resultado));
           }}
         >
           <label className="text-primary600 text-2xl font-medium flex justify-center">
@@ -147,17 +153,103 @@ export default function BMICalculator() {
             epidemiológicas. Conhecida como índice de Quetelet, ela divide o
             peso (em kg) pela altura (em metros) ao quadrado (IMC = peso /
             altura²), sendo um indicativo importante dos riscos de saúde, como
-            doenças cardiovasculares e diabetes.
+            doenças cardiovasculares e diabetes. Para mais detalhes, acesse o
+            estudo completo{" "}
+            <a
+              href="https://www.who.int/publications/i/item/physical-status-the-use-and-interpretation-of-anthropometry"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline"
+            >
+              aqui
+            </a>
+            .
+          </p>
+          <p className="text-primary400 text-sm mt-3">
+            É importante notar que a fórmula para calcular o IMC é a mesma para
+            homens e mulheres, embora a interpretação dos resultados possa
+            variar conforme fatores individuais, como composição corporal e
+            distribuição de gordura.
           </p>
           <p className="text-primary400 text-sm mt-3">
             Nosso site adota essa abordagem validada para oferecer uma
             ferramenta prática e confiável, auxiliando os usuários no
             monitoramento do seu estado nutricional e incentivando escolhas mais
-            saudáveis. Descubra mais sobre os estudos por trás do IMC e os
-            benefícios de manter um estilo de vida equilibrado!
+            saudáveis.
           </p>
         </div>
       </div>
+
+      {result && (
+        <>
+          <div className="max-w-full p-5 bg-primary200 rounded-lg flex gap-5">
+            <div className="dadosResultado">
+              <h2 className="text-primary600 text-xl font-medium mb-3">
+                Resultado:
+              </h2>
+
+              <>
+                <div className="">
+                  <p className="text-primary400 text-medium text-3xl">
+                    IMC
+                    <br />
+                    {JSON.parse(result).imc}
+                  </p>
+                  <p className="text-primary400 text-light text-lg">
+                    Peso: {JSON.parse(result).peso}kg
+                  </p>
+                  <p className="text-primary400 text-light text-lg">
+                    Altura: {JSON.parse(result).altura}m
+                  </p>
+                  <p className="text-primary400 text-light text-lg">
+                    Gênero: {JSON.parse(result).genero}
+                  </p>
+                  <p className="text-primary400 text-light text-lg">
+                    Faixa Etaria: {JSON.parse(result).faixaEtaria}
+                  </p>
+                </div>
+              </>
+            </div>
+            <div className="classificacaoResultado">
+              <div className="">
+                <h2 className="text-primary600 text-xl font-medium mb-3">
+                  Classificação do IMC:
+                </h2>
+                <div className="flex gap-2 max-w-[642px]">
+                  <p className="text-primary400 text-sm">
+                    <strong>18,5 ou menos:</strong> Abaixo do normal - Procure
+                    um médico. Pode ser característica do organismo ou
+                    desnutrição.
+                  </p>
+                  <p className="text-primary400 text-sm mt-2">
+                    <strong>Entre 18,6 e 24,9:</strong> Normal - Mantenha um
+                    estilo de vida ativo e alimentação equilibrada.
+                  </p>
+                  <p className="text-primary400 text-sm mt-2">
+                    <strong>Entre 25,0 e 29,9:</strong> Sobrepeso -
+                    Pré-obesidade. Pode estar associado a diabetes e
+                    hipertensão.
+                  </p>
+                  <p className="text-primary400 text-sm mt-2">
+                    <strong>Entre 30,0 e 34,9:</strong> Obesidade grau I - Sinal
+                    de alerta. Inicie mudanças e acompanhamento com
+                    profissionais.
+                  </p>
+                  <p className="text-primary400 text-sm mt-2">
+                    <strong>Entre 35,0 e 39,9:</strong> Obesidade grau II -
+                    Mudanças no estilo de vida com acompanhamento profissional
+                    são essenciais.
+                  </p>
+                  <p className="text-primary400 text-sm mt-2">
+                    <strong>Acima de 40,0:</strong> Obesidade grau III - Risco
+                    de doenças graves. Tratamento urgente necessário.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
